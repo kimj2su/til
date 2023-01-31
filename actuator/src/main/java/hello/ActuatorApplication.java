3e56b831d3c14e7f41170bb7e1f0ed49a91d5483
kim jisu <jisu3316@naver.com>
2023-01-31 20:19:19 +0900

    HTTP 요청 응답 기록

--- actuator/src/main/java/hello/ActuatorApplication.java
+++ actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,7 +1,9 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ActuatorApplication {
@@ -10,4 +12,14 @@ public class ActuatorApplication {
         SpringApplication.run(ActuatorApplication.class, args);
     }
 
+    /* http://localhost:8080/actuator/httpexchanges 최대 100개의 요청을 제공한다.
+        setCapacity(100); 요청 개수 변경 가능
+        운영에서는 핀포인트, zipkin 사용
+     */
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        InMemoryHttpExchangeRepository inMemoryHttpExchangeRepository = new InMemoryHttpExchangeRepository();
+        inMemoryHttpExchangeRepository.setCapacity(100);
+        return inMemoryHttpExchangeRepository;
+    }
 }
