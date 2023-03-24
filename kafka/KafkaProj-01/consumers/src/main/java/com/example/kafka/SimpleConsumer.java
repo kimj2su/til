00b4da13e98486e1cdb93cb449b2e276aec27f05
kim jisu <jisu3316@naver.com>
2023-03-24 23:24:41 +0900

    Java 기반에서 Consumer 구현

---
+++ kafka/KafkaProj-01/consumers/src/main/java/com/example/kafka/SimpleConsumer.java
@@ -0,0 +1,23 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.List;
+import java.util.Properties;
+
+public class SimpleConsumer {
+    public static void main(String[] args) {
+
+        String topicName = "simple-topic";
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
+        kafkaConsumer.subscribe(List.of(topicName));
+    }
+}
