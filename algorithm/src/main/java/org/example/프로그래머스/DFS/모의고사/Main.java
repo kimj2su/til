2c9cbbfd556cc808d6a69eed26e61956ec348b71
kim jisu <jisu3316@naver.com>
2023-04-10 22:19:19 +0900

    알고리즘풀이

--- algorithm/src/main/java/org/example/프로그래머스/DFS/모의고사/Main.java
+++ algorithm/src/main/java/org/example/프로그래머스/DFS/모의고사/Main.java
@@ -1,9 +1,14 @@
 package org.example.프로그래머스.DFS.모의고사;
 
-import java.util.Arrays;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
+    private static final int[][] RULES = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+    };
 
     public static void main(String[] args) {
         int[] corrects = new int[3];
@@ -18,17 +23,12 @@ public class Main {
                 int picked = getPicked(person, problem);
                 if (answer == picked) {
                     //답과 내가 선택한 답이 같으면 더해준다.
-                    System.out.println("corrects = " + Arrays.toString(corrects));
                     if (++corrects[person] > max) {
-                        System.out.println("corrects = " + Arrays.toString(corrects));
-                        System.out.println("1.max = " + max);
                         max = corrects[person];
-                        System.out.println("2.max = " + max);
                     }
                 }
             }
         }
-        System.out.println("corrects = " + Arrays.toString(corrects));
         final int maxCorrects = max;
         int[] ints = IntStream.range(0, 3)
                 .filter(i -> corrects[i] == maxCorrects)
@@ -37,16 +37,10 @@ public class Main {
         System.out.println("ints = " + Arrays.toString(ints));
     }
 
-    private static final int[][] RULES = {
-            {1, 2, 3, 4, 5},
-            {2, 1, 2, 3, 2, 4, 2, 5},
-            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
-    };
-
     private static int getPicked(int person, int problem) {
         int[] rule = RULES[person];
         int index = problem % rule.length;
-        return rule[problem];
+        return rule[index];
     }
 
 }
