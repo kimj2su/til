975e0aa5db40c57c305bbcf58bc9e915d828bf48
kim jisu <jisu3316@naver.com>
2023-04-17 22:19:19 +0900

    ps

---
+++ algorithm/src/main/java/org/example/inflearn/array/임시반장정하기/Main.java
@@ -0,0 +1,37 @@
+package org.example.inflearn.array.임시반장정하기;
+
+import java.util.Scanner;
+
+public class Main {
+    public int solution(int n, int[][] arr) {
+        int answer = 0, max = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if (cnt > max) {
+                max = cnt;
+                answer = i;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] arr = new int[n + 1][6];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 5; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        Main main = new Main();
+        System.out.println(main.solution(n, arr));
+    }
+}
