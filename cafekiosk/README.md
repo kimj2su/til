6f9b831154926985f72924cada09d62986caee57
kim jisu <jisu3316@naver.com>
2023-05-18 23:28:11 +0900

    Spring JPA 기반 테스트 Presentation Layer 테스트 - 1

--- cafekiosk/README.md
+++ cafekiosk/README.md
@@ -79,6 +79,10 @@ JUnit vs Spock
 - Persistence Layer와의 상호작용(Data를 읽고 쓰는 행위)을 통해 비즈니스 로직을 전개시킨다.
 - 트랜잭션을 보장해야 한다.
 
+## Presentation Layer - Controller test(Business Layer 포함)
+- 외부 세계의 요청을 가장 먼저 받는 계층
+- 파라미터에 대한 최소한의 검증을 수행한다.
+- 
 
 
 
--- cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,21 +2,62 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
-import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
 
 import java.util.List;
 
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
+import static sample.cafekiosk.spring.domain.product.ProductType.*;
+
+/**
+ *  readOnly = true : 읽기 전용
+ *  CRUD 에서 CUD 동작 x / only Read
+ *  JPA : CUD 스냅샷 저장, 변경감지 X (성능 향상 효과가 있다.)
+ *
+ *  CQRS - Command / Query 분리
+ *
+ */
+@Transactional(readOnly = true)
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        // nextProductNumber
+        String nextProductNumber = createNextProductNumber();
+
+        Product product = request.toEntity(nextProductNumber);
+        Product savedProduct = productRepository.save(product);
+
+        return ProductResponse.of(savedProduct);
+    }
+
     public List<ProductResponse> getSellingProducts() {
-        List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+        List<Product> products = productRepository.findAllBySellingStatusIn(forDisplay());
         return products.stream().map(ProductResponse::of).toList();
+    }
+
+    private String createNextProductNumber() {
+        // productNumber
+        // 001 002 003 004
+        // DB에서 마지막 저장된 Product의 상품번호를 읽어와서 + 1
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+//        int nextProductNumber = Integer.parseInt(latestProductNumber) + 1;
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        // 9 -> 009, 10 -> 010
+        return String.format("%03d", nextProductNumberInt);
+
     }
 }
