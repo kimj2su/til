cc805e1fbbf00996c9d28f62e1f956068fb3dc43
kim jisu <jisu3316@naver.com>
2023-05-23 22:19:19 +0900

    백준 대회 or 인턴

---
+++ algorithm/src/main/java/org/example/백준/greedy/대회or인턴/Main.java
@@ -0,0 +1,81 @@
+package org.example.백준.greedy.대회or인턴;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int K;
+    static int[] arr;
+    static String[] str;
+
+    public static void main(String[] args) {
+        input();
+        getResult();
+    }
+
+    private static void getResult() {
+        int result = 0;
+        while (N >= 2 && M >= 1 && N + M >= K + 3) {
+            N -= 2;
+            M -= 1;
+            result++;
+        }
+
+        System.out.println(result);
+    }
+
+    private static void input() {
+        Reader reader = new Reader();
+        N = reader.nextInt();
+        M = reader.nextInt();
+        K = reader.nextInt();
+    }
+
+    static class Reader {
+        BufferedReader br;
+        StringTokenizer st;
+
+
+        public Reader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
