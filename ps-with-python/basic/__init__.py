23caa7adf494cb1e3110ebe117a9fbfee2c3907a
kim jisu <jisu3316@naver.com>
2023-10-14 22:32:19 +0900

    ps

---
+++ ps-with-python/basic/__init__.py
@@ -0,0 +1,25 @@
+print(type(1))
+my_tupe = ('a', 'b', 'c')
+my_arr = ['a', 'b', 'c']
+print(my_tupe[0])
+print(my_arr[0])
+
+print(float(1))
+
+################ String ################
+
+# 포맷팅
+print("포맷팅 : ", 'My name is {}'.format('John'))
+
+# 문자열 인덱싱
+print("문자열 인덱싱", "My name is"[1])
+
+# 문자열 슬라이싱
+print("슬라이싱 : ", "My name is"[1:3])
+
+# split
+print("split : ", "My name is".split(" "))
+
+""" 이것도 주석이다. """
+print("안녕", end = "")
+
