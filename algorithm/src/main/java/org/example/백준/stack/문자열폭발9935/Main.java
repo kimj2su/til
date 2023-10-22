cb8d25da2f6b0dfaf049ca80e8227730ceba2bff
kim jisu <jisu3316@naver.com>
2023-10-22 22:32:19 +0900

    ps

---
+++ algorithm/src/main/java/org/example/백준/stack/문자열폭발9935/Main.java
@@ -0,0 +1,94 @@
+package org.example.백준.stack.문자열폭발9935;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static String str;
+    static String bomb;
+
+    public static void main(String[] args) {
+        input();
+        getResult();
+    }
+
+    private static void getResult() {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+
+            if (stack.size() >= bomb.length()) {
+                boolean isSame = true;
+                for (int j = 0; j < bomb.length(); j++) {
+                    if (stack.get(stack.size() - bomb.length() + j) != bomb.charAt(j)) {
+                        isSame = false;
+                        break;
+                    }
+                }
+
+                if (isSame) {
+                    for (int j = 0; j < bomb.length(); j++)
+                        stack.pop();
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char ch : stack)
+            sb.append(ch);
+
+        System.out.println(sb.length() > 0 ? sb.toString() : "FRULA");
+    }
+
+    private static void input() {
+        Reader reader = new Reader();
+        str = reader.nextLine();
+        bomb = reader.nextLine();
+    }
+
+    static class Reader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public Reader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
