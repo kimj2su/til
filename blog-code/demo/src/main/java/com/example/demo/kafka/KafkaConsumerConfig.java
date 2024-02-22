0a704e1e0bdd58ca1fd4a3c33f6c374bceae89df
kim jisu <kimjisu3268@gmail.com>
2024-02-22 21:30:19 +0900

    Schema Registry 적용 중

--- blog-code/demo/src/main/java/com/example/demo/kafka/KafkaConsumerConfig.java
+++ blog-code/demo/src/main/java/com/example/demo/kafka/KafkaConsumerConfig.java
@@ -1,43 +1,44 @@
-// package com.example.demo.kafka;
-//
-// import com.fasterxml.jackson.databind.ser.std.StringSerializer;
-// import org.apache.kafka.clients.consumer.ConsumerConfig;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-// import org.springframework.kafka.core.ConsumerFactory;
-// import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-//
-// import java.util.HashMap;
-// import java.util.Map;
-//
-// @Configuration
-// public class KafkaConsumerConfig {
-//     @Value("${spring.kafka.consumer.bootstrap-servers}")
-//     private String bootstrapServers;
-//
-//     @Value("${spring.kafka.consumer.auto-offset-reset}")
-//     private String autoOffsetReset;
-//
-//     @Value("${spring.kafka.consumer.enable-auto-commit}")
-//     private boolean enableAutoCommit;
-//
-//     @Bean
-//     public ConsumerFactory<String, String> consumerFactory() {
-//         Map<String, Object> props = new HashMap<>();
-//         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-//         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
-//         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, enableAutoCommit);
-//         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//         return new DefaultKafkaConsumerFactory<>(props);
-//     }
-//
-//     @Bean
-//     ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-//         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-//         factory.setConsumerFactory(consumerFactory());
-//         return factory;
-//     }
-// }
+package com.example.demo.kafka;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String autoOffsetReset;
+
+    @Value("${spring.kafka.consumer.enable-auto-commit}")
+    private boolean enableAutoCommit;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, enableAutoCommit);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
