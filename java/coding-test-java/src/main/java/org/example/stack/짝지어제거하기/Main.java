34d3cd51b63782b2d5fbbf97a31fca49dd31b4bb
kim jisu <kimjisu3268@gmail.com>
2024-04-17 23:47:19 +0900

    stack - 짝지어 제거하기

---
+++ java/coding-test-java/src/main/java/org/example/stack/짝지어제거하기/Main.java
@@ -0,0 +1,25 @@
+package org.example.stack.짝지어제거하기;
+
+import java.util.Stack;
+
+class Main {
+
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // 1. 스택이 비어 있지 않고 현재 문자와 스택의 맨 위 문자가 같으면
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop(); // 2. 스택의 맨 위 문자 제거
+            } else {
+                stack.push(c); // 3. 스택엔 현재 문자 추가
+            }
+        }
+        return stack.isEmpty() ? 1: 0; // 4. 스택이 비어 있으면 1 그렇지 않으면 0 반환
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.solution("baabaa")); // 1
+    }
+}
---
+++ java/coding-test-java/src/main/java/org/example/stack/짝지어제거하기/README.md
@@ -0,0 +1,4 @@
+# 짝지어 제거하기
+
+## [문제](https://programmers.co.kr/learn/courses/30/lessons/12973)
+
