c856b45f8c992658559426562d7519ebb43e2401
kim jisu <kimjisu3268@gmail.com>
2024-04-28 22:47:19 +0900

    카프카 스키마 레지스트리 실습

---
+++ kafka/schema-registry/src/main/java/com/jisu/AvroConsumer1.java
@@ -0,0 +1,47 @@
+package com.jisu;
+
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import student.avro.Student;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class AvroConsumer1 {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "http//localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "avro-consumer-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put("specific.avro.reader", "true");
+
+        // KafkaConsumer 생성
+        KafkaConsumer<String, Student> consumer = new KafkaConsumer<>(props);
+
+        // 구독할 토픽 설정
+        consumer.subscribe(Collections.singletonList("peter-avro2"));
+
+        // 메시지 소비 루프
+        try {
+            while (true) {
+                ConsumerRecords<String, Student> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, Student> record : records) {
+                    System.out.printf("Consumed message: topic = %s, partition = %s, offset = %s, key = %s, value = %s%n",
+                            record.topic(), record.partition(), record.offset(), record.key(), record.value());
+                }
+            }
+        } finally {
+            consumer.close();
+        }
+    }
+}
---
+++ kafka/schema-registry/src/main/java/com/jisu/AvroProducer1.java
@@ -0,0 +1,63 @@
+package com.jisu;
+
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class AvroProducer1 {
+    public static void main(String[] args) {
+        // Avro 스키마 정의
+        String schemaString = "{"
+                + "\"namespace\": \"student.avro\","
+                + "\"type\": \"record\","
+                + "\"doc\": \"This is an example of Avro.\","
+                + "\"name\": \"Student\","
+                + "\"fields\": ["
+                + "{\"name\": \"name\", \"type\": [\"null\", \"string\"], \"default\": null, \"doc\": \"Name of the student\"},"
+                + "{\"name\": \"class\", \"type\": \"int\", \"default\": 1, \"doc\": \"Class of the student\"}"
+                + "]"
+                + "}";
+
+        Schema schema = new Schema.Parser().parse(schemaString);
+
+        // Kafka producer properties 설정
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
+
+        // KafkaProducer 생성
+        KafkaProducer<String, GenericRecord> producer = new KafkaProducer<>(props);
+
+        // Avro 레코드 생성
+        GenericRecord student = new GenericData.Record(schema);
+        student.put("name", "Peter");
+        student.put("class", 1);
+
+        // ProducerRecord 생성
+        ProducerRecord<String, GenericRecord> record = new ProducerRecord<>("peter-avro2", student);
+
+        // 메시지 전송 및 결과 확인
+        try {
+            RecordMetadata metadata = producer.send(record).get();
+            System.out.printf("Message sent to topic:%s partition:%s  offset:%s%n",
+                    metadata.topic(), metadata.partition(), metadata.offset());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Producer 닫기
+        producer.close();
+    }
+}
