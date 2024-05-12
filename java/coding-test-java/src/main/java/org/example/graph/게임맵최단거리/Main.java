fe92a656c45573e7accb097881379890f151dbe2
kim jisu <kimjisu3268@gmail.com>
2024-05-12 22:47:19 +0900

    bfs 너비우선 탐색

--- java/coding-test-java/src/main/java/org/example/graph/게임맵최단거리/Main.java
+++ java/coding-test-java/src/main/java/org/example/graph/게임맵최단거리/Main.java
@@ -2,6 +2,11 @@ package org.example.graph.게임맵최단거리;
 
 import java.util.ArrayDeque;
 
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/1844
+ * 배열의 크기가 N * M 이라 하면 dis 배열을 초기화할 때의 시간 복잡도는 O(N*M)이고,
+ * 너비 우선 탐색을 할 때는 최악의 경우 dist의 모든 위치가 큐에 들어가는 경우이므로 너비 우선 탐색의 시간 복잡도는 O(N*M)이다.
+ */
 public class Main {
 
     private static final int[] rx = {0, 0, 1, -1};
@@ -45,10 +50,12 @@ public class Main {
                 int nr = r + rx[i];
                 int nc = c + ry[i];
 
+                // 범위를 벗어나는 경우 무시
                 if (nr < 0 || nr >= N || nc < 0 || nc >= M) {
                     continue;
                 }
 
+                // maps[nr][nc]가 0이면 벽이므로 무시, dist[nr][nc]가 0이 아니면 이미 방문한 곳이므로 무시
                 if (maps[nr][nc] == 0 || dist[nr][nc] != 0) {
                     continue;
                 }
@@ -57,6 +64,7 @@ public class Main {
                 queue.addLast(new Node(nr, nc));
             }
         }
+        // 목적지까지 최단 거리 반환, 도달하지 못했으면 -1 반환
         return dist[N - 1][M - 1] == 0 ? -1 : dist[N - 1][M - 1];
     }
     public static void main(String[] args) {
