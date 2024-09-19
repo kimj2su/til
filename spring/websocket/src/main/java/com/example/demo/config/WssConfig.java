e3c8a669e1ebacd1a63a3632d0a32959611f390d
kim jisu <kimjisu3268@gmail.com>
2024-09-20 00:39:57 +0900

    stomp

--- spring/websocket/src/main/java/com/example/demo/config/WssConfig.java
+++ spring/websocket/src/main/java/com/example/demo/config/WssConfig.java
@@ -2,37 +2,34 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WssConfig implements WebSocketConfigurer {
+public class WssConfig implements WebSocketMessageBrokerConfigurer {
 
-//    @Override
-//    public void configureMessageBroker(MessageBrokerRegistry registry) {
-//        registry.enableSimpleBroker("/sub");
-//        registry.setApplicationDestinationPrefixes("/pub");
-//    }
-//
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry register ) {
-//        register.addEndpoint("/ws-stomp")
-//            .setAllowedOrigins("*");
-//    }
+  // pub/sub 메시지를 처리할 endpoint 설정
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.enableSimpleBroker("/sub");
+    registry.setApplicationDestinationPrefixes("/pub");
+  }
+
+  // 연결되는 base endpoint 설정
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry register) {
+    register.addEndpoint("/ws-stomp")
+        .setAllowedOrigins("*");
+    // .withSockJS();
+  }
+
+//     @Override
+//     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//         registry.addHandler(null, "/ws/v1/chat")
+//             .setAllowedOrigins("*");
+//     }
 
 
-     @Override
-     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-         registry.addHandler(null, "/ws/v1/chat")
-             .setAllowedOrigins("*");
-     }
-
-    
 }
